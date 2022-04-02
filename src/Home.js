9e673d4c36f4a3482bd9b4647d253aac92510a55
Gustavo <gustavoalfonso27@gmail.com>
2022-04-02 15:50:17 -0400

    first commit

---
+++ src/Home.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import food from "./img/food.svg"
+import grocery from "./img/grocery.svg"
+
+export const Home = () => {
+  return (
+    <div>
+        <div className="header">
+        <div className="blur"></div>
+        <img className='backgroundImageHeader' src={require("./img/lucrezia-carnelos-wQ9VuP_Njr4-unsplash.jpg")} alt="" srcset="" />
+            <h2>Si tienes <b>MeneStore</b>, tienes todo.</h2>
+            <div className="search">
+                <input type="text" placeholder='Busca tu tienda favorita:'/> 
+                <button> 
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                    </svg>
+                </button>
+                <div className="location"><i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z" clip-rule="evenodd" />
+                </svg></i>Ver tiendas cerca de ti</div>
+            </div>
+        </div>
+        <div className="category">
+            <div className="shop">
+                <img src={grocery}/>
+                <p>Comprar comida<i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg></i></p>
+            </div>
+            <div className="restaurant">
+                <img src={food}/>
+                <p>Ordenar Comida <i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg></i></p>
+            </div>
+        </div>
+    </div>
+  )
+}
---
+++ src/Navbar.js
@@ -0,0 +1,45 @@
+import React from 'react'
+import './App.css';
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="logo">
+        <h1>MeneStore</h1>
+      </div>
+      <div className="search">
+        <input type="text" placeholder='Buscar en MeneStore:'/>
+        <button> 
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+          <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+        </svg>
+        </button>
+      </div>
+      <div className="links">
+        <a><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z" />
+              </svg></i>
+              Inicio</a>
+        <a><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clip-rule="evenodd" />
+        </svg></i> Tiendas</a>
+        <a><i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M18 3a1 1 0 00-1.447-.894L8.763 6H5a3 3 0 000 6h.28l1.771 5.316A1 1 0 008 18h1a1 1 0 001-1v-4.382l6.553 3.276A1 1 0 0018 15V3z" clip-rule="evenodd" />
+        </svg>
+          </i> Vender</a>
+      </div>
+      <div className="login">
+        <a className='signIn'>
+        <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd" />
+        </svg></i> Iniciar sesión</a>
+        <a className='signUp'>
+        <i><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+          <path d="M8 9a3 3 0 100-6 3 3 0 000 6zM8 11a6 6 0 016 6H2a6 6 0 016-6zM16 7a1 1 0 10-2 0v1h-1a1 1 0 100 2h1v1a1 1 0 102 0v-1h1a1 1 0 100-2h-1V7z" />
+        </svg></i>Registrarse</a>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
