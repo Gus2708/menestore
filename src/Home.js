80fcd157d8711c6130f0c56fbce8c9a7aa4f463c
Gus2708 <scarygamer08@gmail.com>
2022-04-02 17:51:42 -0400

    homepage

--- src/Home.js
+++ src/Home.js
@@ -6,8 +6,8 @@ export const Home = () => {
   return (
     <div>
         <div className="header">
-        <div className="blur"></div>
-        <img className='backgroundImageHeader' src={require("./img/lucrezia-carnelos-wQ9VuP_Njr4-unsplash.jpg")} alt="" srcset="" />
+            <div className="blur"></div>
+            <img className='backgroundImageHeader' src={require("./img/lucrezia-carnelos-wQ9VuP_Njr4-unsplash.jpg")} alt="" srcset="" />
             <h2>Si tienes <b>MeneStore</b>, tienes todo.</h2>
             <div className="search">
                 <input type="text" placeholder='Busca tu tienda favorita:'/> 
@@ -24,7 +24,7 @@ export const Home = () => {
         <div className="category">
             <div className="shop">
                 <img src={grocery}/>
-                <p>Comprar comida<i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                <p>Comprar productos<i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
                     <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg></i></p>
             </div>
@@ -34,6 +34,17 @@ export const Home = () => {
                     <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
                 </svg></i></p>
             </div>
+        </div>
+        <div className="about">
+            <img src={require("./img/istockphoto-1138671697-612x612.jpg")}/>
+            <div className="text">
+                <h2>La mejor opcion para impulsar tu negocio.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Id, dolor ratione alias nobis itaque eius magni nesciunt voluptas nam culpa iure voluptate, quasi eos amet cum, nisi in provident excepturi. Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium similique corrupti quibusdam necessitatibus tenetur vero eius vel? Natus, nostrum architecto quo saepe soluta beatae incidunt debitis nobis tempore molestiae illo.</p>
+                <a>Convertirse en vendedor <i><svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg></i></a>
+            </div>
+
         </div>
     </div>
   )
