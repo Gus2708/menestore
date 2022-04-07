82e03cb6fa26f1abcef08629035173e5975d44c1
Gustavo Reyes <gustavoalfonso27@gmail.com>
2022-04-07 16:46:47 -0400

    Login page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
 import { Footer } from './Footer';
 import { Home } from './Home';
+import { SignIn } from './SignIn';
 import Navbar from './Navbar';
 
 function App() {
   return (
   <div>
     <Navbar/>
-    <Home/>
+    <SignIn/>
     <Footer/>
   </div>
   );
---
+++ src/SignIn.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import login from "./img/login.svg"
+
+export const SignIn = () => {
+  return (
+    <div className='loginPage'>
+      <div className="svg">
+        <img src={login} alt="" srcset="" />
+      </div>
+      <div className="container">
+        <h2>Inicia sesión</h2>
+        <div className="form">
+          <div className="email">
+            <h3>Ingresa tu email:</h3>
+            <input className='textInput' type="email" placeholder='ejemplo@menestore.com'/>
+          </div>
+          <div className="password">
+            <h3>Ingresa tu contraseña:</h3>
+            <input className='textInput' type="password" placeholder='Contraseña'/>
+          </div>
+        </div>
+        <div className="buttons">
+          <button className='signInButton'>Iniciar sesion</button>
+        </div>
+      </div>
+    </div>
+  )
+}
